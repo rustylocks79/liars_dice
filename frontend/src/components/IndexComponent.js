0efe73d4f6afb65f10d88489922d94ad7c9a5670
bdwarner99 <bdwarner99@gmail.com>
2021-02-12 15:21:54 -0500

    added links to IndexComponent.js

--- frontend/src/components/IndexComponent.js
+++ frontend/src/components/IndexComponent.js
@@ -1,5 +1,5 @@
 import {withCookies} from "react-cookie";
-import {withRouter} from "react-router-dom";
+import {Link, withRouter} from "react-router-dom";
 import React from "react";
 
 
@@ -8,6 +8,15 @@ class IndexComponent extends React.Component {
         return (
             <div>
                 <p>Index Component</p>
+
+
+                <Link to={"/login"}>Login Screen</Link> <br/>
+                <Link to={"/signup"}>Signup Screen</Link> <br/>
+                <Link to={"/welcome"}>Welcome Screen</Link> <br/>
+                <Link to={"/profile"}>Profile Screen</Link> <br/>
+                <Link to={"/joingame"}>Join Game Screen</Link> <br/>
+                <Link to={"/lobby"}>Lobby Screen</Link> <br/>
+                <Link to={"/game"}>Game Screen</Link> <br/>
             </div>
         );
     }
