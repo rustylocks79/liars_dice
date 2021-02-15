2235cc5b6e4328afb5030653a857b1c0f7e2dfc3
nhNetz <nathuntnet@yahoo.com>
2021-02-15 16:41:30 -0500

    Created heuristic agent

--- test_agents.py
+++ test_agents.py
@@ -1,3 +1,4 @@
+import math
 import pickle
 
 from liars_dice import LiarsDice
@@ -23,20 +24,46 @@ class PlayerAgent (Agent):
         else:
             return (action, )
 
-# class HeuristicAgent (Agent):
-#     def get_action(self, game, index:int):
-#         if game.bid_history == []:
-#             face = rand.randint(2,6)
-#             num_face = 1.0/3*game.
-#         last_quantity = game.bid_history[-1][1]
-#         last_face = game.bid_history[-1][2]
+class HeuristicAgent (Agent):
+    def get_action(self, game, index:int):
+        num_unknown = sum([len(hand) for hand in game.hands]) - len(game.hands[index])
+        if not game.bid_history:
+            face = rand.randint(2,6)
+            num_face_in_hand = game.hands[index].count(face) + game.hands[index].count(1)
+            quantity = math.floor(num_face_in_hand + 1.0/3*num_unknown)
+            if quantity == 0:
+                quantity += 1
+            return "raise", quantity, face
+        else:
+            last_quantity = game.bid_history[-1][1]
+            last_face = game.bid_history[-1][2]
+            num_face_in_hand = game.hands[index].count(last_face) + game.hands[index].count(1)
+            expected_value = math.floor(num_face_in_hand + 1.0/3*num_unknown)
+            if last_quantity > expected_value:
+                return ("doubt", )
+            else:
+                quantity = last_quantity + 1
+                return "raise", quantity, last_face
 
 
+
+
+# if __name__ == '__main__':
+#     game = LiarsDice(3, 5)
+#     strategy = pickle.load(open('strategies/liars_dice.pickle', 'rb'))
+#     print("-------\nTesting Phase")
+#     agents_stats = game.test(1, [PlayerAgent(), LDStrategyAgent(strategy), LDStrategyAgent(strategy)], True)
+#     for player in range(len(agents_stats)):
+#         print("\tAgent: {}".format(player))
+#         for key in agents_stats[player].keys():
+#             print("\t\t{}: {}".format(key, agents_stats[player][key]))
+
+# LDStrategy vs. Heuristic
 if __name__ == '__main__':
-    game = LiarsDice(3, 5)
+    game = LiarsDice(2, 5)
     strategy = pickle.load(open('strategies/liars_dice.pickle', 'rb'))
     print("-------\nTesting Phase")
-    agents_stats = game.test(1, [PlayerAgent(), LDStrategyAgent(strategy), LDStrategyAgent(strategy)], True)
+    agents_stats = game.test(100, [LDStrategyAgent(strategy), HeuristicAgent()], True)
     for player in range(len(agents_stats)):
         print("\tAgent: {}".format(player))
         for key in agents_stats[player].keys():
