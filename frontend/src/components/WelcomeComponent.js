867a7e1ac3b2d334b319ac7d96159778cb8ab303
bdwarner99 <bdwarner99@gmail.com>
2021-02-22 14:51:40 -0500

    reformatted WelcomeComponent.js text

--- frontend/src/components/WelcomeComponent.js
+++ frontend/src/components/WelcomeComponent.js
@@ -36,32 +36,34 @@ class WelcomeComponent extends React.Component {
                         </Grid>
                         <Grid item xs={6}>
                             <Typography paragraph={true} align={"justify"}>
-                                <p>At the start of a game of Liar’s Dice, each player is given between 1 and 5 dice.</p>
-                                <p>Now each game of Liar’s Dice consists of multiple rounds.</p>
-                                <p>A round starts with each player rolling their dice and concealing them from the other
-                                    players. One of the
-                                    players is chosen at random to start the first round.</p>
-                                <p>The player who goes first must set an initial bid. The bid must consist of a face
-                                    value (between 2 and 6) and a
-                                    quantity (greater than or equal to 1).</p>
-                                <p>The game proceeds in a clockwise direction.</p>
-                                <p>During a player’s turn they can either raise the previous bid or doubt the previous
-                                    bid. If the player chooses to raise the previous bid by choosing a face value and
-                                    quantity.</p>
-                                <p>For a raise to be valid the face value must be greater than the previous
-                                    bid’s face value and with equal quantity or a greater quantity.</p>
-                                <p>If the player doubts
-                                    the previous then all players reveal their dice. The number of dice with the face
-                                    value equal to the previous bid are added up. </p>
-                                <p>Additionally, any dice that rolls a 1 are included since 1 are wild. If there are
-                                    more than or exactly the number of dice
-                                    in play as the quantity then the player who doubted the bid losses a die.</p>
-                                <p>If there are less dice in play than the quantity of the previous bid then the player
-                                    who made the
-                                    previous bid loses a die.</p>
-                                <p>The next round starts with the player who lost the die. If
-                                    the player who lost the die was eliminated then the next player starts the
-                                    bidding.</p>
+                                At the start of a game of Liar’s Dice, each player is given between 1 and 5
+                                dice.<br/><br/>
+                                Now each game of Liar’s Dice consists of multiple rounds.<br/><br/>
+                                A round starts with each player rolling their dice and concealing them from the other
+                                players. One of the
+                                players is chosen at random to start the first round.<br/><br/>
+                                The player who goes first must set an initial bid. The bid must consist of a face
+                                value (between 2 and 6) and a
+                                quantity (greater than or equal to 1).<br/><br/>
+                                The game proceeds in a clockwise direction.<br/><br/>
+                                During a player’s turn they can either raise the previous bid or doubt the previous
+                                bid. If the player chooses to raise the previous bid by choosing a face value and
+                                quantity.<br/><br/>
+                                For a raise to be valid the face value must be greater than the previous
+                                bid’s face value and with equal quantity or a greater quantity.<br/><br/>
+                                If the player doubts
+                                the previous then all players reveal their dice. The number of dice with the face
+                                value equal to the previous bid are added up. <br/><br/>
+                                Additionally, any dice that rolls a 1 are included since 1 are wild. If there are
+                                more than or exactly the number of dice
+                                in play as the quantity then the player who doubted the bid losses a die.<br/><br/>
+                                If there are less dice in play than the quantity of the previous bid then the player
+                                who made the
+                                previous bid loses a die.<br/><br/>
+                                previous bid loses a die.<br/><br/>
+                                The next round starts with the player who lost the die. If
+                                the player who lost the die was eliminated then the next player starts the
+                                bidding.
                             </Typography>
                         </Grid>
                         <Grid item xs={6}>
