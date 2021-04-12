6915f3a35458826de9ac03a60b54f453d7f8a9b6
bdwarner99 <bdwarner99@gmail.com>
2021-04-11 20:41:31 -0400

    added player hand display to game

--- frontend/src/components/GameComponent.js
+++ frontend/src/components/GameComponent.js
@@ -2,6 +2,11 @@ import {withCookies} from "react-cookie";
 import {withRouter} from "react-router-dom";
 import React from "react";
 import {connect} from "react-redux";
+import {
+    GiDiceSixFacesOne, GiDiceSixFacesTwo, GiDiceSixFacesThree,
+    GiDiceSixFacesFour, GiDiceSixFacesFive, GiDiceSixFacesSix
+} from "react-icons/gi";
+import {Grid} from "@material-ui/core";
 
 class GameComponent extends React.Component {
     state = {
@@ -10,14 +15,72 @@ class GameComponent extends React.Component {
 
     constructor(props) {
         super(props);
+    }
 
+    displayHand = () => {
+        let diceNum = 0;
+        let handDisplay = []
+
+        for (let i = 0; i < this.props.hand[0]; i++) {
+            diceNum++
+            handDisplay.push(<GiDiceSixFacesOne key={diceNum}
+                                                style={{height: "6vmin", width: "6vmin", verticalAlign: "middle"}}/>)
+        }
+        for (let i = 0; i < this.props.hand[1]; i++) {
+            diceNum++
+            handDisplay.push(<GiDiceSixFacesTwo key={diceNum}
+                                                style={{height: "6vmin", width: "6vmin", verticalAlign: "middle"}}/>)
+        }
+        for (let i = 0; i < this.props.hand[2]; i++) {
+            diceNum++
+            handDisplay.push(<GiDiceSixFacesThree key={diceNum}
+                                                  style={{height: "6vmin", width: "6vmin", verticalAlign: "middle"}}/>)
+        }
+        for (let i = 0; i < this.props.hand[3]; i++) {
+            diceNum++
+            handDisplay.push(<GiDiceSixFacesFour key={diceNum}
+                                                 style={{height: "6vmin", width: "6vmin", verticalAlign: "middle"}}/>)
+        }
+        for (let i = 0; i < this.props.hand[4]; i++) {
+            diceNum++
+            handDisplay.push(<GiDiceSixFacesFive key={diceNum}
+                                                 style={{height: "6vmin", width: "6vmin", verticalAlign: "middle"}}/>)
+        }
+        for (let i = 0; i < this.props.hand[5]; i++) {
+            diceNum++
+            handDisplay.push(<GiDiceSixFacesSix key={diceNum}
+                                                style={{height: "6vmin", width: "6vmin", verticalAlign: "middle"}}/>)
+        }
+
+        return handDisplay
     }
 
     render() {
         return (
             <div style={{height: '500px'}}>
-                <p>Game Component</p>
-                <p>Hand: {JSON.stringify(this.props.hand)}</p>
+                <div style={{height: '70%'}}>
+                    <Grid
+                        container
+                        direction="row"
+                        justify="space-evenly"
+                        alignItems="flex-start"
+                        spacing={1}
+                    >
+                        <Grid item xs={2}>
+                            <p>Hello</p>
+                        </Grid>
+                        <Grid item xs={2}>
+                            <p>World</p>
+                        </Grid>
+                    </Grid>
+                </div>
+                <div style={{textAlign: "center"}}>
+                    <h3>My Hand</h3>
+                    <div>
+                        {this.displayHand()}
+                    </div>
+
+                </div>
             </div>
         );
     }
@@ -26,7 +89,10 @@ class GameComponent extends React.Component {
 const mapStateToProps = state => {
     return {
         socket: state.socket,
-        hand: state.hand
+        hand: state.hand,
+        players: state.players,
+        bots: state.bots,
+        activeDice: state.activeDice
     }
 }
 
