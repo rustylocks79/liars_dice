60250848b3575186f06c5f3c603206db1b98198c
bdwarner99 <bdwarner99@gmail.com>
2021-04-19 15:43:24 -0400

    updated some temporary stuff on about screen

--- frontend/src/components/PromotionalWebsite/PromotionalAboutComponent.js
+++ frontend/src/components/PromotionalWebsite/PromotionalAboutComponent.js
@@ -6,7 +6,35 @@ function PromotionalAboutComponent() {
     return (
         <div>
             <PromotionalTopBarComponent/>
-            <h1>PromotionalAboutComponent</h1>
+            <div style={{textAlign: "left", width: "80%", margin: "auto"}}>
+                <h4>See: <a
+                    href={'https://github.com/CTOverton/CMPSC_488/blob/master/promotional-website/about.html'}>here</a> for
+                    title and description examples</h4>
+                <br/>
+
+                <h2>Jeremy Dellock</h2>
+                <h3>ENTER TITLE</h3>
+                <p>ENTER BRIEF PERSONAL PARAGRAPH</p>
+                <br/>
+
+                <h2>Nathaniel Netznik</h2>
+                <h3>ENTER TITLE</h3>
+                <p>ENTER BRIEF PERSONAL PARAGRAPH</p>
+                <br/>
+
+                <h2>Long Nguyen</h2>
+                <h3>ENTER TITLE</h3>
+                <p>ENTER BRIEF PERSONAL PARAGRAPH</p>
+                <br/>
+
+                <h2>Ben Warner</h2>
+                <h3>ENTER TITLE</h3>
+                <p>ENTER BRIEF PERSONAL PARAGRAPH</p>
+                <br/>
+
+                <p style={{textAlign: "center", fontSize: "xx-large"}}>Check out the
+                    <a href={'https://github.com/rustylocks79/liars_dice'}>Project Github!</a></p>
+            </div>
         </div>
     )
 }
